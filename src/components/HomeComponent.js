a22ff50b2bc60d95fe57cf643a4b2e8934cbf4cb
Oliver Squibb <osquibb@gmail.com>
2019-04-10 14:18:05 -0400

    icons added

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import { Row, Col, Jumbotron } from 'reactstrap';
+import { Jumbotron, ListGroup, ListGroupItem, ListGroupItemText } from 'reactstrap';
 
+const IDEAS = [ {id: 1, text: 'a great idea!'},
+								{id: 2, text: 'a great idea!'},
+								{id: 3, text: 'a great idea!'},
+								{id: 4, text: 'a great idea!'},
+								{id: 5, text: 'a great idea!'},
+								{id: 6, text: 'a great idea!'},
+								{id: 7, text: 'a great idea!'},
+								{id: 8, text: 'a great idea!'},
+								{id: 9, text: 'a great idea!'},
+								{id: 10, text: 'a great idea!'},
+								{id: 11, text: 'a great idea!'},
+								{id: 12, text: 'a great idea!'},
+								{id: 13, text: 'a great idea!'},
+								{id: 14, text: 'a great idea!'},
+								{id: 15, text: 'a great idea!'}
+						];
+
+function Ideas(props) {
+	return props.ideas.map(idea => 
+	<ListGroupItem key={idea.id}>
+	<a className="fa fa-thumbs-down fa-2x mr-4 text-danger"></a>
+	<a className="fa fa-thumbs-up fa-2x mr-4 text-success"></a>
+	{idea.text}
+	<a className="fa fa-ban fa-2x float-right text-warning"></a>
+	</ListGroupItem>);
+}
 
 export default class Home extends Component {
 	render() {
 		return(
-				<Jumbotron className="mt-3" >
-					
+				<Jumbotron className="mt-3">
+					<ListGroup>
+						<Ideas ideas={IDEAS} />
+					</ListGroup>
 				</Jumbotron>
     );
 	}
