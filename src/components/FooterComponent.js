16d064856fcb0cdca1b6d2004540016d5ed97872
Oliver Squibb <osquibb@gmail.com>
2019-06-27 16:36:56 -0400

    footer margin

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,7 +4,7 @@ import { Row, Col } from 'reactstrap';
 export default class Footer extends Component {
 	render() {
 		return(
-			<Row>
+			<Row className="mt-3">
 				<Col className="text-center">
 				&copy; o+k
 				</Col>
